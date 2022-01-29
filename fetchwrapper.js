6afe431f3a8657f9465c195ac7a688bc70f3d711
Gravedd <troperread@gmail.com>
2022-01-29 15:01:02 +0400

    1.0 v

--- fetchwrapper.js
+++ fetchwrapper.js
@@ -1,4 +1,13 @@
+//GET FETCH
+//receives data from the server in json format
+//the function takes the url address
+async function fetchget(url){
+    let response = await fetch(url);
+    return await response.json();
+}
 //POST FETCH
+//sends a json and returns a response from the server in json format
+//the function takes the url address, and the object to be sent to the server
 async function fetchpost(url, senddata){
     let response = await fetch(url, {
         method: 'POST',
@@ -6,15 +15,10 @@ async function fetchpost(url, senddata){
         headers: {
             "Accept": "application/json",
             "X-Requested-With": "XMLHttpRequest",
-            // "X-CSRF-Token": token.value,     ---this for laravel
+            // "X-CSRF-Token": token.value,//this for laravel
             'Content-Type': 'application/json;charset=utf-8'
         },
         body: JSON.stringify(senddata)//send data
     });
-    return await response.json();
-}
-//GET FETCH
-async function fetchget(url){
-    let response = await fetch(url);
     return await response.json();
 }
