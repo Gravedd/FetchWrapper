da79fd86d2c2ffa00a3a1d6edf33e1ad4bbb6a79
Gravedd <troperread@gmail.com>
2022-01-29 12:24:41 +0400

    first working version

--- fetchwrapper.js
+++ fetchwrapper.js
@@ -1,16 +1,20 @@
+//POST FETCH
 async function fetchpost(url, senddata){
     let response = await fetch(url, {
-        method: 'GET',
+        method: 'POST',
         credentials: "same-origin",//with cookies
         headers: {
-            /*"Accept": "application/json",
-            "X-Requested-With": "XMLHttpRequest",*/
+            "Accept": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
             // "X-CSRF-Token": token.value,     ---this for laravel
             'Content-Type': 'application/json;charset=utf-8'
         },
-            // body: JSON.stringify(senddata)//send data
+        body: JSON.stringify(senddata)//send data
     });
-    let content = await response.json();
-    console.log(content)
-    return content;
+    return await response.json();
+}
+//GET FETCH
+async function fetchget(url){
+    let response = await fetch(url);
+    return await response.json();
 }
